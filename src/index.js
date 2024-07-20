c333d70d416dd1ae8d6eabf6df4c0b935ce1fdf0
nick <nicksanchez2371@gmail.com>
2024-07-19 23:44:36 -0500

    board size

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import { createBoard} from "./minesweeper";
 import { test2 } from "./test3";
 import "./style.css"
 
-console.log(createBoard(2, 2))
+const BOARD_SIZE = 2
+const NUMBER_OF_MINES = 2
+
+const board = (createBoard(BOARD_SIZE, NUMBER_OF_MINES))
+const boardElement = document.querySelector('.board')
+
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element)
+    })
+})
+boardElement.style.setProperty('--size', BOARD_SIZE)
 
 // 1. Populate a board with tiles/mines
 // 2. left click on tiles
